9f0d9299537f6769a5aee4bd44cb01ab088f4dc8
Brian Mock <brian@mockbrian.com>
2017-10-23 19:40:15 -0700

    Stripping it down for Firefox

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const isFirefox = /Firefox\/[^ ]+/.test(navigator.userAgent);
+const isChrome = /Chrome\/[^ ]+/.test(navigator.userAgent);
+
+function setBodyClass() {
+  let bodyClass = undefined;
+  if (isFirefox) {
+    bodyClass = "IS_FIREFOX";
+  } else if (isChrome) {
+    bodyClass = "IS_CHROME";
+  } else {
+    return;
+  }
+  document.body.classList.add(bodyClass);
+}
+
 function throttle(time, fn) {
   let lastRun = Date.now();
   return () => {
@@ -24,7 +39,7 @@ function E(type, props, children) {
   children = children || [];
   const elem = document.createElement(type);
   Object.keys(props).forEach(k => {
-    if (k === 'style') {
+    if (k === "style") {
       Object.assign(elem.style, props[k]);
     } else {
       elem[k] = props[k];
@@ -32,7 +47,7 @@ function E(type, props, children) {
   });
   children
     .map(asNode)
-    .filter(x => x !== null)
+    .filter(x => x)
     .forEach(kid => {
       elem.appendChild(kid);
     });
@@ -40,34 +55,46 @@ function E(type, props, children) {
 }
 
 function asNode(x) {
-  if (typeof x === 'string') {
+  if (typeof x === "string") {
     return document.createTextNode(x);
   } else {
     return x;
   }
 }
 
-function faviconImageSet(url) {
-  return `
-    -webkit-image-set(
-      url(chrome://favicon/size/16@1x/${url}) 1x,
-      url(chrome://favicon/size/16@2x/${url}) 2x)
-  `;
+function faviconUrl(url) {
+  if (isChrome) {
+    return `
+      -webkit-image-set(
+        url(chrome://favicon/size/16@1x/${url}) 1x,
+        url(chrome://favicon/size/16@2x/${url}) 2x)
+    `;
+  } else if (isFirefox) {
+    const parsedUrl = new URL(url);
+    parsedUrl.pathname = "favicon.ico";
+    console.log(parsedUrl.href);
+    return parsedUrl.href;
+  }
 }
 
 function img(url) {
-  return E('span', {
-    className: 'FaviconImage',
-    style: {backgroundImage: faviconImageSet(url)}
-  }, []);
+  if (isChrome) {
+    const opts = {
+      className: "FaviconImage",
+      style: { backgroundImage: faviconUrl(url) }
+    };
+    return E("span", opts, []);
+  } else if (isFirefox) {
+    return E("img", { className: "FaviconImage", src: faviconUrl(url) }, []);
+  }
 }
 
 function favicon(url) {
-  return E('span', {className: 'Favicon va-middle'}, [img(url)]);
+  return E("span", { className: "Favicon va-middle" }, [img(url)]);
 }
 
 function title(title) {
-  return E('span', {className: 'va-middle'}, [title]);
+  return E("span", { className: "va-middle" }, [title]);
 }
 
 function walkBookmarks(node, callback, path) {
@@ -81,50 +108,67 @@ function walkBookmarks(node, callback, path) {
       walkBookmarks(child, callback, (path || []).concat(node.title));
     });
   } else {
-    throw new Error('What kind of bookmark node is this?');
+    throw new Error("What kind of bookmark node is this?");
   }
 }
 
 function generatePath(path) {
   const twins = path.map(chunk => [
-    E('span', {className: 'gray'}, [chunk]),
-    E('span', {className: 'light-gray'}, [' / '])
+    E("span", { className: "gray" }, [chunk]),
+    E("span", { className: "light-gray" }, [" / "])
   ]);
   const kids = twins.reduce((a, b) => a.concat(b));
-  return E('span', {className: 'va-middle'}, kids);
+  return E("span", { className: "va-middle" }, kids);
 }
 
 function generateLink(site, path) {
   const kids = [
-    favicon(site.url),
-    path ? generatePath(path) : null,
-    title(site.title)
+    isChrome ? favicon(site.url) : undefined,
+    path ? generatePath(path) : undefined,
+    title(site.title || site.url)
   ];
-  return E('a', {className: 'Bookmark db reset-link', href: site.url}, kids);
+  return E("a", { className: "Bookmark db reset-link", href: site.url }, kids);
+}
+
+function bookmarksBarId() {
+  if (isFirefox) {
+    return "toolbar_____";
+    // return "menu________";
+  } else if (isChrome) {
+    return "1";
+  }
+}
+
+function bookmarksOtherId() {
+  if (isFirefox) {
+    return "unfiled_____";
+  } else if (isChrome) {
+    return "2";
+  }
 }
 
 function render() {
-  if (!tabs['top-sites'] || !tabs.bookmarks || !tabs.others) {
+  if (!tabs["top-sites"] || !tabs.bookmarks || !tabs.others) {
     return;
   }
-  tabs['top-sites'].innerHTML = '';
+  tabs["top-sites"].innerHTML = "";
   chrome.topSites.get(sites => {
     sites.forEach(site => {
-      tabs['top-sites'].appendChild(generateLink(site));
+      tabs["top-sites"].appendChild(generateLink(site));
     });
   });
-  renderBookmarksSubtreeByIdInto('1', tabs.bookmarks);
-  renderBookmarksSubtreeByIdInto('2', tabs.others);
+  renderBookmarksSubtreeByIdInto(bookmarksBarId(), tabs.bookmarks);
+  renderBookmarksSubtreeByIdInto(bookmarksOtherId(), tabs.others);
 }
 
 function emptyMessage() {
-  return E('p', {className: 'tc gray'}, [
-    'This is where bookmarks would go, if you had any'
+  return E("p", { className: "tc gray" }, [
+    "This is where bookmarks would go, if you had any"
   ]);
 }
 
 function renderBookmarksSubtreeByIdInto(bookmarkId, rootElem) {
-  rootElem.innerHTML = '';
+  rootElem.innerHTML = "";
   chrome.bookmarks.getSubTree(bookmarkId, nodes => {
     if (nodes[0].children.length === 0) {
       rootElem.appendChild(emptyMessage());
@@ -142,9 +186,9 @@ function renderBookmarksSubtreeByIdInto(bookmarkId, rootElem) {
 const tabHandlers = {};
 
 const tabs = {
-  bookmarks: null,
-  'top-sites': null,
-  others: null,
+  bookmarks: undefined,
+  "top-sites": undefined,
+  others: undefined
 };
 
 function eachPair(obj, callback) {
@@ -153,19 +197,19 @@ function eachPair(obj, callback) {
 
 eachPair(tabs, (_obj, k) => {
   tabHandlers[k] = () => {
-    localStorage.setItem('last_tab', k);
+    localStorage.setItem("last_tab", k);
     eachPair(tabs, (_obj, k2) => {
-      ID('show-' + k2).classList.remove('is-current');
-      tabs[k2].style.display = 'none';
+      ID("show-" + k2).classList.remove("is-current");
+      tabs[k2].style.display = "none";
     });
-    ID('show-' + k).classList.add('is-current');
-    tabs[k].style.display = '';
+    ID("show-" + k).classList.add("is-current");
+    tabs[k].style.display = "";
   };
 });
 
 function newTabOpener(url) {
   return event => {
-    chrome.tabs.create({url});
+    chrome.tabs.create({ url });
     window.close();
     if (event) {
       event.preventDefault();
@@ -173,24 +217,25 @@ function newTabOpener(url) {
   };
 }
 
-listen(window, 'DOMContentLoaded', () => {
+listen(window, "DOMContentLoaded", () => {
+  setBodyClass();
   eachPair(tabs, (obj, k) => {
     obj[k] = ID(k);
-    listen(ID('show-' + k), 'click', event => {
+    listen(ID("show-" + k), "click", event => {
       event.preventDefault();
       tabHandlers[k]();
     });
   });
   const newTabLinks = [
-    ID('edit-bookmarks'),
-    ID('go-to-apps'),
-    ID('go-to-extensions'),
+    ID("edit-bookmarks"),
+    ID("go-to-apps"),
+    ID("go-to-extensions")
   ];
   newTabLinks.forEach(link => {
     link.onclick = newTabOpener(link.dataset.href);
   });
   // Restore last focused tab
-  const tab = localStorage.getItem('last_tab') || 'bookmarks';
+  const tab = localStorage.getItem("last_tab") || "bookmarks";
   const fun = tabHandlers[tab];
   if (fun) {
     fun();
@@ -199,7 +244,7 @@ listen(window, 'DOMContentLoaded', () => {
   const throttledRender = throttle(200, render);
   // Re-render on all bookmarks update events
   Object.keys(chrome.bookmarks)
-    .filter(name => name.indexOf('on') === 0)
+    .filter(name => name.indexOf("on") === 0)
     .forEach(name => {
       chrome.bookmarks[name].addListener(throttledRender);
     });
